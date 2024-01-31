4a24d256d3b9469e472d24afb0cb7eaff475cea6
MariaShamsutdinova <Shamsutdinovamari@gmail.com>
2024-01-31 15:37:05 +0500

    Вот теперь точно добавила абстрактный класс и метод

---
+++ varseven_calc/src/varseven_calc/Material.java
@@ -0,0 +1,14 @@
+package varseven_calc;
+
+public abstract class Material {
+	private double cost;
+	public abstract void generation();
+	
+	public double getCost() {
+		return cost;
+	}
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+}
